dd7814585ea7e05dc8ef406ce2301424398fa6f0
WaqarAhamadWD <wa8287683@gmail.com>
2024-12-16 03:56:01 +0500

    updated

--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,7 @@ const db = {};
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USERNAME,
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
